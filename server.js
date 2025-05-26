703b8546532f52a7be5766760e6ceabdf396516d
Code Masters <palmiflor56@gmail.com>
2025-05-26 10:50:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+    chromeLocation: '/usr/bin/chromium-browser',
+    chromeFlags: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=9222'
+    ]
+});
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
-// server.use(prerender.blockResources());
 server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
 const port = process.env.PORT || 3000;
 server.start({ port });
-
 
 
