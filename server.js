539c4a7135b046140b7a17677bfc84e8d98181f7
Code Masters <palmiflor56@gmail.com>
2025-05-26 08:17:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,15 @@
 #!/usr/bin/env node
+var prerender = require('./lib');
 
-// 🧠 Requiere Puppeteer y le da a Prerender la ruta del Chrome embebido
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-process.env.PUPPETEER_SKIP_DOWNLOAD = 'true';
-process.env.PUPPETEER_EXECUTABLE_PATH = process.env.CHROME_BIN;
-process.env.PRERENDER_CHROME_FLAGS = '--no-sandbox --disable-setuid-sandbox';
+var server = prerender();
 
-const prerender = require('./lib');
-const server = prerender();
-
-const PORT = process.env.PORT || 10000;
-
-// --------------------
-// CACHE EN MEMORIA
-// --------------------
-const cache = new Map();
-const CACHE_DURATION_MS = 5 * 60 * 1000; // 5 minutos
-
-function getCacheKey(req) {
-  return req.url;
-}
-
-function isCacheValid(entry) {
-  return Date.now() - entry.timestamp < CACHE_DURATION_MS;
-}
-
-server.use({
-  requestReceived: (req, res, next) => {
-    const key = getCacheKey(req);
-    const entry = cache.get(key);
-
-    if (entry && isCacheValid(entry)) {
-      console.log(`[CACHE] HIT: ${key}`);
-      res.send(entry.html);
-    } else {
-      res.send = ((originalSend) => (html) => {
-        console.log(`[CACHE] STORE: ${key}`);
-        cache.set(key, { html, timestamp: Date.now() });
-        originalSend.call(res, html);
-      })(res.send);
-
-      next();
-    }
-  }
-});
-
-// --------------------
-// Otros middlewares
-// --------------------
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
+// server.use(prerender.blockResources());
 server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
-// --------------------
-// Iniciar servidor
-// --------------------
-server.start({ port: PORT, host: '0.0.0.0' });
+server.start();
 
 
